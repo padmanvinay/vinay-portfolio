7e6ce3f4dc3f5a280f5fcaccf309cca21ed50d35
Vinay Padman <vinaypadman111@gmail.com>
2024-03-05 19:23:20 +0530

    Add react-toastify for displaying notifications

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -3,10 +3,29 @@ import LinkedinIcon from "../../assets/linkedin.svg?react";
 import GithubIcon from "../../assets/github.svg?react";
 import GmailIcon from "../../assets/gmail.svg?react";
 import PhoneIcon from "../../assets/phone.svg?react";
+import { ToastContainer } from "react-toastify";
 
 const Contact = () => {
+  const buttonHandler = (location: string) => {
+    switch (location) {
+      case "linkedin":
+        window.open("https://in.linkedin.com/in/vinaypadman");
+        break;
+      case "github":
+        window.open("https://github.com/padmanvinay");
+        break;
+      case "gmail":
+        navigator.clipboard.writeText("vinaypadman111@gmail.com");
+        break;
+      case "phone":
+        navigator.clipboard.writeText("+91-9745781390");
+        break;
+    }
+  };
+
   return (
     <>
+      <ToastContainer />
       <div className={styles.contact}>
         <div>CONTACTS</div>
         <div className={styles.contact_buttons}>
@@ -14,25 +33,25 @@ const Contact = () => {
             <div className={styles.icon}>
               <LinkedinIcon />
             </div>
-            <button>LINKEDIN</button>
+            <button onClick={() => buttonHandler("linkedin")}>LINKEDIN</button>
           </div>
           <div className={styles.contact_container}>
             <div className={styles.icon}>
               <GithubIcon />
             </div>
-            <button>GITHUB</button>
+            <button onClick={() => buttonHandler("github")}>GITHUB</button>
           </div>
           <div className={styles.contact_container}>
             <div className={styles.icon}>
               <GmailIcon />
             </div>
-            <button>E-MAIL</button>
+            <button onClick={() => buttonHandler("gmail")}>E-MAIL</button>
           </div>
           <div className={styles.contact_container}>
             <div className={styles.icon}>
               <PhoneIcon />
             </div>
-            <button>PHONE</button>
+            <button onClick={() => buttonHandler("github")}>PHONE</button>
           </div>
         </div>
       </div>
