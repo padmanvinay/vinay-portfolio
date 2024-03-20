392a86a41e5adc196b73f856bb44859c0838a6e3
Vinay Padman <vinaypadman111@gmail.com>
2024-03-20 19:36:28 +0530

    Update index.tsx

--- src/components/UI/Sparkles/index.tsx
+++ src/components/UI/Sparkles/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use client";
-import React from "react";
+
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import type { Container, SingleOrMultiple } from "@tsparticles/engine";
